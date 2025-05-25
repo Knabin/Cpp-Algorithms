af26fc7fa52741fdbf819b5ad68ae0181cb3948f
Nabin Kim <knabinn97@gmail.com>
2025-05-25 17:24:05 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string input;
+    cin >> input;
+
+    cout << input << "\?\?\!";
+
+    return 0;
+}
